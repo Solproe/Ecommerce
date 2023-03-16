ed556873b91e75fd0ebe0983d4ab9d2d98cc6e40
Solproe <iperez_fragozo@live.com>
2023-03-16 15:40:11 -0500

    datass

--- resources/views/livewire/category-products.blade.php
+++ resources/views/livewire/category-products.blade.php
@@ -2,28 +2,27 @@
     @if (count($products))
 
         <div class="glider-contain">
-            <ul class="glider-{{$category->id}}">
+            <ul class="glider-{{ $category->id }}">
 
                 @foreach ($products as $product)
-
                     <li class="bg-white rounded-lg shadow {{ $loop->last ? '' : 'sm:mr-4' }}">
                         <article>
                             <figure>
-                                <img class="object-cover object-center w-full h-48" src="{{ Storage::url($product->images->first()->url) }}" alt="">
+                                <img class="object-cover object-center w-full h-48"
+                                    src="{{ Storage::url($product->images->first()->url) }}" alt="">
                             </figure>
 
-                        <div class="px-6 py-4">
+                            <div class="px-6 py-4">
                                 <h1 class="text-lg font-semibold">
                                     <a href="{{ route('products.show', $product) }}">
-                                        {{Str::limit($product->name, 20)}}
+                                        {{ Str::limit($product->name, 20) }}
                                     </a>
                                 </h1>
 
-                                <p class="font-bold text-trueGray-700">COP {{$product->price}}</p>
-                        </div>
+                                <p class="font-bold text-trueGray-700">COP {{ $product->price }}</p>
+                            </div>
                         </article>
                     </li>
-
                 @endforeach
 
             </ul>
@@ -32,9 +31,7 @@
             <button aria-label="Next" class="glider-next">»</button>
             <div role="tablist" class="dots"></div>
         </div>
-
     @else
-
         <div class="flex items-center justify-center h-48 mb-4 bg-white border border-gray-100 rounded-lg shadow-xl">
             <div class="w-10 h-10 duration-300 border-2 border-indigo-500 rounded animate-spin ease"></div>
         </div>
